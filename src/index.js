a9a9f6db60050ce9764d4d018bc0bd7815cfcb1a
BlackPri0r <lawrenceli630@gmail.com>
2025-10-22 23:53:19 +1300

    CHORE: added express

--- src/index.js
+++ src/index.js
@@ -36,3 +36,10 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Bot is running"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
